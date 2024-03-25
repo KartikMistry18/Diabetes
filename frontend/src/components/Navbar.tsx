feeac163eb367dab71a4bbbf677d5bff88c94bf1
Om Bhambale <ombhamble@gmail.com>
2024-03-25 17:52:18 +0530

    home page changed , logout button changed in navbar
    logout buttonn now only shows if the user is logged in

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import Link from "next/link"
+import React from 'react';
+import Link from 'next/link';
 import { useClerk } from '@clerk/clerk-react';
 
 const Navbar = () => {
-  const { signOut } = useClerk();
+  const { user, signOut } = useClerk(); // Destructure user from useClerk()
+
   return (
     <>
-
-     <nav style={{ backgroundColor: '#1E40AF' }} className="py-4 px-6 flex items-center justify-between mb-5">
+      <nav style={{ backgroundColor: '#1E40AF' }} className="py-4 px-6 flex items-center justify-between mb-5">
         <div className="flex item-center">
-            <Link href="/">
-                <div className="text-lg uppercase font-bold text-black">DiabPro</div>
-            </Link>
+          <Link href="/">
+            <div className="text-lg font-bold text-white">DiabPro</div>
+          </Link>
         </div>
         <div className="text-white">
-          <Link href="sign-in" className='text-gray-300 hover:text-white mr-4'>Home</Link>
+          <Link href="/index" className="text-gray-300 hover:text-white mr-4">Home</Link>
+          <Link href="/diabetes" className="text-gray-300 hover:text-white mr-4">Diabetes</Link>
+          <Link href="" className="text-gray-300 hover:text-white mr-4">Diabetes Retinopathy</Link>
         </div>
         <div>
-        <Link href="sign-in" className='text-gray-300 hover:text-white mr-4'>Sign In</Link>
-          <Link href="sign-up" className='text-gray-300 hover:text-white mr-4'>Sign Up</Link>
-          <button onClick={() => signOut()} className="text-white font-semibold bg-red-600 px-4 py-2 rounded-md">Logout</button>
+          {user ? ( // Check if user object exists
+            <button onClick={() => signOut()} className="text-white font-semibold bg-red-600 px-4 py-2 rounded-md">Logout</button>
+          ) : (
+            <>
+              <Link href="/sign-in" className="text-gray-300 hover:text-white mr-4">Sign In</Link>
+              <Link href="/sign-up" className="text-gray-300 hover:text-white mr-4">Sign Up</Link>
+            </>
+          )}
         </div>
-     </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
