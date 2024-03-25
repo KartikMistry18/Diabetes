8e09c490b1bcaddba5cebc96f28b253ed4745cb3
Om Bhambale <ombhamble@gmail.com>
2024-03-25 18:37:22 +0530

    added a oneliner on homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,6 +6,7 @@ function Home() {
       
       <main className="py-20 text-center">
         <h1 className="text-4xl font-bold mb-4">DiabPro: Your Diabetes Management Companion</h1>
+        <p className="text-lg mb-8">Diabetes checkup at your fingertips, because your health is in your hands. Check your diabetes status from the comfort of your home, with just a click!</p>
         <p className="text-lg mb-8">Predict Diabetes Risk and Detect Retinopathy Early</p>
         <a href="/diabetes" className="inline-block px-6 py-3 bg-blue-500 text-white text-lg rounded-md no-underline transition-colors duration-300 hover:bg-blue-700">Get Started</a>
       </main>
