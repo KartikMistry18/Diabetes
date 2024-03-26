004a1c6fa35aa5592d7f9d309a53482422f5fb59
Krishay Nair <krishay958@gmail.com>
2024-03-26 09:03:53 +0530

    changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,9 +16,9 @@ const Navbar = () => {
         <div className="text-white">
           <Link href="/index" className="text-gray-300 hover:text-white mr-4">Home</Link>
           <Link href="/diabetes" className="text-gray-300 hover:text-white mr-4">Diabetes</Link>
-          {user && (
+         
             <Link href="https://diabetesretinopathy.streamlit.app/" className="text-gray-300 hover:text-white mr-4">Diabetes Retinopathy</Link>
-          )}
+          
           {user && (
             <Link href="https://diabfordoc.streamlit.app/" className="text-gray-300 hover:text-white mr-4">Stats for doctors</Link>
           )}
@@ -28,8 +28,7 @@ const Navbar = () => {
             <button onClick={() => signOut()} className="text-white font-semibold bg-red-600 px-4 py-2 rounded-md">Logout</button>
           ) : (
             <>
-              <Link href="/sign-in" className="text-gray-300 hover:text-white mr-4">Sign In</Link>
-              <Link href="/sign-up" className="text-gray-300 hover:text-white mr-4">Sign Up</Link>
+              <Link href="/sign-in" className="text-gray-300 hover:text-white mr-4">Sign In / Sign Up for doctors  </Link>
             </>
           )}
         </div>
